30e592008ba47372f0d3a0e7d40b4797adc540ca
chuboot <chuboot.id@gmail.com>
2023-02-04 07:39:42 +0800

    add swiper js for slide -begin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 import "./style.css";
 
 const getCards = async () => {
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,14 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(config, {
   mode: "development",
-  watch: true,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    compress: true,
+    port: 9000,
+    liveReload: true,
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
